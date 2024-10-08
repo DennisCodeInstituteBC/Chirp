721c154b31d7b806808fea3873acf0ca5406c5ec
Justin Evans <justin.robert.evans@protonmail.com>
2024-10-08 11:08:17 +0000

    add signals to models.py JREVANS 08-10-2024 1208

--- chirpsocial/models.py
+++ chirpsocial/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -11,3 +12,16 @@ class Profile(models.Model):
         symmetrical=False,
         blank=True
     )
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+# Create a Profile for each new user.
+post_save.connect(create_profile, sender=User)
+
+
