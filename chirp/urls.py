9f005acb8c21602d74e89f5a54968a26379869a2
Dennis Limbu <87oyrsom@students.codeinstitute.net>
2024-10-08 12:17:04 +0000

    adding base.html and main.css, adding in static in settings, and urls.py + views.py for base.html

--- chirp/urls.py
+++ chirp/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("dwitter.urls")),
+    path("", include("chirpsocial.urls")),
     path('admin/', admin.site.urls),
 ]
